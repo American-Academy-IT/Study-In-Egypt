9de53cd8c421905dc10242dc4aad88f785d499e5
front end <frontend@americanone.local>
2023-07-23 15:42:06 +0300

    updated

--- js/main.js
+++ js/main.js
@@ -1,31 +1,30 @@
 (function ($) {
-    "use strict";
+  "use strict";
+    
+  // Initiate the wowjs
+  new WOW().init();
 
-    
-    // Initiate the wowjs
-    new WOW().init();
 
-
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.sticky-top').css('top', '0px');
-        } else {
-            $('.sticky-top').css('top', '-100px');
-        }
-    });
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $('.sticky-top').css('top', '0px');
+    } else {
+      $('.sticky-top').css('top', '-100px');
+    }
+  });
     
     
-    // Dropdown on mouse hover
-    const $dropdown = $(".dropdown");
-    const $dropdownToggle = $(".dropdown-toggle");
-    const $dropdownMenu = $(".dropdown-menu");
-    const showClass = "show";
+  // Dropdown on mouse hover
+  const $dropdown = $(".dropdown");
+  const $dropdownToggle = $(".dropdown-toggle");
+  const $dropdownMenu = $(".dropdown-menu");
+  const showClass = "show";
     
-    $(window).on("load resize", function() {
-        if (this.matchMedia("(min-width: 992px)").matches) {
-            $dropdown.hover(
-            function() {
+  $(window).on("load resize", function() {
+    if (this.matchMedia("(min-width: 992px)").matches) {
+      $dropdown.hover(
+        function() {
                 const $this = $(this);
                 $this.addClass(showClass);
                 $this.find($dropdownToggle).attr("aria-expanded", "true");
@@ -41,63 +40,46 @@
         } else {
             $dropdown.off("mouseenter mouseleave");
         }
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
+  });
+
+
+  // Header carousel
+  $(".header-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    items: 1,
+    dots: false,
+    loop: true,
+    nav : true,
+    navText : [
+      '<i class="fa-solid fa-arrow-left"></i>',
+      '<i class="fa-solid fa-arrow-right"></i>'
+      ]
+  });
+
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: true,
+    margin: 24,
+    dots: true,
+    loop: true,
+    nav : false,
+    responsive: {
+      0:{
+          items:1
+        },
+        768:{
+          items:2
+        },
+        992:{
+          items:3
         }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-
-
-    // Header carousel
-    $(".header-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        items: 1,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ]
-    });
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        center: true,
-        margin: 24,
-        dots: true,
-        loop: true,
-        nav : false,
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-    
+      }
+  });
 })(jQuery);
-
-
 
 const accordion = document.getElementsByClassName('container');
 
@@ -106,3 +88,24 @@ for (i=0; i<accordion.length; i++) {
     this.classList.toggle('active')
   })
 }
+
+
+let span = document.querySelector(".up");
+
+window.onscroll = function () {
+    console.log(this.scrollY);
+
+    if (this.scrollY >= 1000) {
+        span.classList.add("show");
+    } else {
+        span.classList.remove("show");
+    }
+}
+
+
+span.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+};
